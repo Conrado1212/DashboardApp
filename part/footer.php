ba4d04750141741a202997aa5b394bb70c4a391c
Conrado1212 <conrado1212@gmail.com>
2022-06-11 21:47:22 +0200

    add web parts

---
+++ part/footer.php
@@ -0,0 +1,7 @@
+<section class="Footer">
+    <div class="conteiner">
+        <div class="footer">DashboardApp.com &copy; 2022 Thank you for your visit</div>
+</div>
+</section>
+</body>
+</html>
---
+++ part/menu.php
@@ -0,0 +1,30 @@
+<div class="doc"></div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="css/fontello.css" rel="stylesheet" type="text/css" />
+    <title>Main page</title>
+</head>
+<body>
+           <div class="title">
+           <a href="index.php">
+            <span style="color: #808080">DashBoard</span>App.com
+               </a>
+           </div>
+    <div class="menu">
+        <div class="left">
+        <nav class="navbar">
+            <div class="titleMenu">Menu</div>
+            <ul>
+                <li><a href="controls.html">HTML</a></li>
+                <li><a href="employees.html">Employees</a></li>
+                <li><a href="vat.html">Invoices Vat</a></li>
+                <li><a href="delegation.html">Delegation</a></li>
+                <li><a href="contractors.html">Contractors</a></li>
+            </ul>
+        </nav>
+        </div>
